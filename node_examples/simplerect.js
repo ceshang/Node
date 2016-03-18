1db3a0bc101e0bb366cf0098b68ce8ec8556fc6b
CeShang <shangce919@hotmail.com>
2016-03-18 01:17:59 -0500

    first nodejs example

---
+++ node_examples/simplerect.js
@@ -0,0 +1,27 @@
+/**
+ * Created by CeShang on 2016/3/17.
+ */
+var rect = require('rect');
+
+var argv = require('yargs')
+    .usage('Usage:node $0 --l=[num] --b=[num]')
+    .demand(['l','b'])
+    .argv;
+
+function solveRect(l,b){
+    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+
+    rect(l,b,function(err,rectangle) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("the area of a rectangle of dimensions length = " + l +
+                " and breadth = " + b + " is " + rectangle.area(l, b));
+            console.log("the perimeter of a rectangle of dimensions length = "
+                + l + " and breadth = " + b + " is " + rectangle.perimeter(l, b));
+        }
+    });
+}
+
+solveRect(argv.l,argv.b);
